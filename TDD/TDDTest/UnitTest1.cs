ed31a3b5ce82dcd01cbf734e1cd01e6bbcc0b9f7
이현우 <kakakzkz@naver.com>
2017-09-26 22:06:10 +0900

    Make testmethod TestMultipleCation

--- TDD/TDDTest/UnitTest1.cs
+++ TDD/TDDTest/UnitTest1.cs
@@ -7,8 +7,11 @@ namespace TDDTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMultiplication()
         {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.Amount);
         }
     }
 }
